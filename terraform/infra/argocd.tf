966271f3df8aec851970e6cabd277908c7c9f1d7
samarthlambture <lambture83@gmail.com>
2026-06-17 13:54:13 +0000

    removing security-groups

--- terraform/infra/argocd.tf
+++ terraform/infra/argocd.tf
@@ -12,15 +12,19 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   version    = "7.1.3" # Uses a stable, production-ready chart version
   namespace  = kubernetes_namespace.argocd.metadata[0].name
+}
+
+
+
+
 
   # Optional but recommended configuration overrides
-#   set {
-#     name  = "server.service.type"
-#     value = "ClusterIP" # Keep it internal; we will expose it securely later via Ingress
-#   }
+  # set {
+  #   name  = "server.service.type"
+  #   value = "ClusterIP" # Keep it internal; we will expose it securely later via Ingress
+  # }
 
-#   set {
-#     name  = "configs.secret.argocdServerAdminPasswordMuted"
-#     value = "false"
-#   }
-}
+  # set {
+  #   name  = "configs.secret.argocdServerAdminPasswordMuted"
+  #   value = "false"
+  # }
