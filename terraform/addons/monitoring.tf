b6dbf6ee1789c65523d33074cf9d2bc1fe1e2187
samarthlambture <lambture83@gmail.com>
2026-06-24 15:58:35 +0000

    feat: add Prometheus scraping + Grafana dashboard

--- terraform/addons/monitoring.tf
+++ terraform/addons/monitoring.tf
@@ -1,3 +1,33 @@
+# resource "kubernetes_namespace" "monitoring" {
+#   metadata {
+#     name = "monitoring"
+#   }
+# }
+
+# resource "helm_release" "prometheus_stack" {
+#   name       = "prometheus"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   version    = "61.3.0" # Stable tracking version
+#   namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+#   # For a development/learning setup, we can disable persistent storage 
+#   # so it doesn't try to provision expensive AWS EBS volumes right away.
+#   set {
+#     name  = "prometheus.prometheusSpec.storageSpec"
+#     value = ""
+#   }
+
+#   # Ensure Grafana is enabled and ready
+#   set {
+#     name  = "grafana.enabled"
+#     value = "true"
+#   }
+
+#   depends_on = [data.aws_eks_cluster.this]
+# }
+
+
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
@@ -8,20 +38,77 @@ resource "helm_release" "prometheus_stack" {
   name       = "prometheus"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  version    = "61.3.0" # Stable tracking version
+  version    = "61.3.0"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
-  # For a development/learning setup, we can disable persistent storage 
-  # so it doesn't try to provision expensive AWS EBS volumes right away.
+  # Allow Prometheus to discover ServiceMonitors in ALL namespaces (including ecommerce-app-ns)
+  set {
+    name  = "prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues"
+    value = "false"
+  }
+  set {
+    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+    value = "false"
+  }
+  set {
+    name  = "prometheus.prometheusSpec.ruleSelectorNilUsesHelmValues"
+    value = "false"
+  }
+
+  # Expose Grafana via AWS NLB so you can open it in a browser
+  set {
+    name  = "grafana.service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "grafana.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "external"
+  }
+  set {
+    name  = "grafana.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
+    value = "ip"
+  }
+  set {
+    name  = "grafana.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
+
+  # Grafana admin credentials
+  set {
+    name  = "grafana.adminUser"
+    value = "admin"
+  }
+  set_sensitive {
+    name  = "grafana.adminPassword"
+    value = "EcommerceGrafana2024!"   # Change this to something strong
+  }
+
+  # Tell Grafana to auto-load dashboards from ConfigMaps that have the label grafana_dashboard=1
+  set {
+    name  = "grafana.sidecar.dashboards.enabled"
+    value = "true"
+  }
+  set {
+    name  = "grafana.sidecar.dashboards.label"
+    value = "grafana_dashboard"
+  }
+  set {
+    name  = "grafana.sidecar.dashboards.labelValue"
+    value = "1"
+  }
+  set {
+    name  = "grafana.sidecar.dashboards.searchNamespace"
+    value = "ALL"   # Picks up ConfigMaps from any namespace
+  }
+
+  # Disable persistent storage for dev (avoids EBS cost)
   set {
     name  = "prometheus.prometheusSpec.storageSpec"
     value = ""
   }
-
-  # Ensure Grafana is enabled and ready
   set {
-    name  = "grafana.enabled"
-    value = "true"
+    name  = "grafana.persistence.enabled"
+    value = "false"
   }
 
   depends_on = [data.aws_eks_cluster.this]
